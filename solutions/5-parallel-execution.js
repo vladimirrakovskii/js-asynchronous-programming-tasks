3bf02867d25c566daf9fc9a9b5bc83d17a409717
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 23:49:07 +0300

    Update 5-parallel-execution.js

--- solutions/5-parallel-execution.js
+++ solutions/5-parallel-execution.js
@@ -4,5 +4,33 @@ import _ from 'lodash';
 import async from 'async';
 
 // BEGIN
-
+export const getDirectorySize = (dirPath, cbDir) => {
+    fs.readdir(dirPath, (err, files) => {
+      if (err) {
+        cbDir(err);
+        return;
+      }
+      const filePaths = files.map(file => path.join(dirPath, file));
+      async.map(filePaths, (filePath, cbFile) => {
+        fs.stat(filePath, (errStat, stats) => {
+          if (errStat) {
+            cbFile(errStat);
+            return;
+          }
+          if (stats.isFile()) {
+            cbFile(null, stats.size);
+          } else {
+            cbFile(null, 0);
+          }
+        });
+      }, (errMap, res) => {
+        if (errMap) {
+          cbDir(errMap);
+          return;
+        }
+	const sumSize = _.sumBy(res, size => size);
+        cbDir(null, sumSize);
+      });
+    });
+  };
 // END
