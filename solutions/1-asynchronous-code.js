9412aaac331ba5a8b76c3e2e2510d14c559a62f5
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 23:44:26 +0300

    Update 1-asynchronous-code.js

--- solutions/1-asynchronous-code.js
+++ solutions/1-asynchronous-code.js
@@ -1,5 +1,8 @@
 import fs from 'fs';
 
 // BEGIN
-
+export default (filePath) => {
+    const callback = (_error, data) => console.log(data)
+    fs.readFile(filePath, 'utf-8', callback);
+};
 // END
