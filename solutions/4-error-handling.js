3f33e784ff22a519b1d374d16481f731c400de9f
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 23:48:25 +0300

    Update 4-error-handling.js

--- solutions/4-error-handling.js
+++ solutions/4-error-handling.js
@@ -1,5 +1,25 @@
 import fs from 'fs';
 
 // BEGIN
-
+export const move = (source, destination, callback) => {
+  fs.readFile(source, (err, data) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+    fs.writeFile(destination, data, (err) => {
+      if (err) {
+        callback(err);
+        return;
+      }
+      fs.unlink(source, (err) => {
+        if (err) {
+          callback(err);
+          return;
+        }
+        callback(null);
+      });
+    });
+  });
+};
 // END
