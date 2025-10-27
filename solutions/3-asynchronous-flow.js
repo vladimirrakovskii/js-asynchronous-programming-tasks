0dcd8f9169c3dcad5f9b0ae16edd201ac9d5f6f2
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 23:46:31 +0300

    Update 3-asynchronous-flow.js

--- solutions/3-asynchronous-flow.js
+++ solutions/3-asynchronous-flow.js
@@ -1,5 +1,22 @@
 import fs from 'fs';
 
 // BEGIN
-
+export const compareFileSizes = (filepath1, filepath2, callback) => {
+  fs.stat(filepath1, (err1, stats1) => {
+    if (err1) {
+      callback(err1, null);
+      return;
+    }
+    fs.stat(filepath2, (err2, stats2) => {
+      if (err2) {
+        callback(err2, null);
+        return;
+      }
+      const size1 = stats1.size;
+      const size2 = stats2.size;
+      const result = Math.sign(size1 - size2);
+      callback(null, result);
+    });
+  });
+};
 // END
