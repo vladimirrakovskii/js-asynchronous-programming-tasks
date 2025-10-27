32150f25e5b909ca2206d3b24fb44a32649abef1
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 23:49:48 +0300

    Update 6-timers.js

--- solutions/6-timers.js
+++ solutions/6-timers.js
@@ -1,5 +1,24 @@
 import fs from 'fs';
 
 // BEGIN
-
+export default (filePath, period, cb) => {
+    let lastTime = null;
+    let timerId = setInterval(() => {
+      fs.stat(filePath, (err, stats) => {
+        if (err) {
+          clearInterval(timerId);
+          cb(err);
+          return;
+        }
+        const modified = stats.mtimeMs;
+        if (lastTime === null) {
+            lastTime = modified;
+        } else if (modified > lastTime) {
+            lastTime = modified;
+          cb(null);
+        }
+      });
+    }, period);
+    return timerId;
+  };
 // END
