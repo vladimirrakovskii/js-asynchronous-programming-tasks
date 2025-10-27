0a789c62a5f0a87a0bcb04d3c5c792c9d20e3dfe
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 23:44:56 +0300

    Update 2-asynchronous-return.js

--- solutions/2-asynchronous-return.js
+++ solutions/2-asynchronous-return.js
@@ -1,5 +1,10 @@
 import fs from 'fs';
 
 // BEGIN
-
+export default (filePath, data, cb) => {
+    fs.writeFile(filePath, data, (err) => {
+        if (!(err))
+            cb();
+    })
+};
 // END
