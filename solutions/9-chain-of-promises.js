68ed38a7ad875c1b9505d474e5ad0eab34de87dd
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 23:52:01 +0300

    Update 9-chain-of-promises.js

--- solutions/9-chain-of-promises.js
+++ solutions/9-chain-of-promises.js
@@ -1,5 +1,12 @@
 import fsp from 'fs/promises';
 
 // BEGIN
-
+export const getTypes = (paths) => {
+  const promises = paths.map((path) =>
+    fs.promises.stat(path)
+      .then((stat) => (stat.isDirectory() ? 'directory' : 'file'))
+      .catch(() => null)
+  );
+  return Promise.all(promises);
+};
 // END
