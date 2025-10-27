324be38f2e31a2cdca29379c2b37b2f7ce376528
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 23:50:29 +0300

    Update 7-promises.js

--- solutions/7-promises.js
+++ solutions/7-promises.js
@@ -1,5 +1,9 @@
 import fsp from 'fs/promises';
 
 // BEGIN
-
+export const reverse = async (filePath) => {
+    const data = await fsp.readFile(filePath, 'utf-8');
+    const revData = data.split('\n').reverse().join('\n');
+    await fsp.writeFile(filePath, revData, 'utf-8');
+  };
 // END
