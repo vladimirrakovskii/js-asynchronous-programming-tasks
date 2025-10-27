58ee5859cec897a1940d1cb2daa9c5106b6bf101
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 23:50:54 +0300

    Update 8-promises-catch.js

--- solutions/8-promises-catch.js
+++ solutions/8-promises-catch.js
@@ -1,5 +1,13 @@
 import fsp from 'fs/promises';
 
 // BEGIN
-
+export const touch = (filePath) => {
+    return fsp.access(filePath)
+      .then(() => {
+        return Promise.resolve();
+      })
+      .catch(() => {
+        return fsp.writeFile(filePath, '');
+      });
+  };
 // END
